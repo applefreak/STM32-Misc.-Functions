774d99e374040ce17329fe84d795b3f52ae5018c
applefreak <p820316@gmail.com>
2013-04-18 19:22:43 +0800

    Added Files

---
+++ comFunc/comFunc.h
@@ -0,0 +1,11 @@
+#include "stm32f10x.h"
+#include "stm32f10x_gpio.h"
+
+
+/**  comFunc_Exported_Functions
+  *
+  */
+void initToggle(void);
+void PB10Toggle(void);
+void PB11Toggle(void);
+void Delay(uint32_t nCount);
